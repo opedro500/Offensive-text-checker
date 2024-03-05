30117cd2ab61b3c380339f3f38ef3befa5dbb8d7
Pedro Cavalcanti <pedropjc500@gmail.com>
2024-03-05 18:09:13 -0300

    Versão 1.0 final

---
+++ src/javascript/script.js
@@ -0,0 +1,48 @@
+document.querySelector('#send').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const text = document.querySelector('#verify_text').value;
+
+    if (!text) {
+        removeClass();
+        document.querySelector('#error_comment').classList.add('show');
+
+        return;
+    }
+
+    const key = "AIzaSyBPEowkUcyUHjH8NT7mW1OAnDW3uGMH7SQ";
+    const url = `https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=${key}`;
+
+    const data = JSON.stringify({
+        comment: { text: text },
+        languages: ["pt"],
+        requestedAttributes: { TOXICITY: {} }
+    });
+
+    fetch(url, {
+        method: "POST",
+        body: data,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.attributeScores && data.attributeScores.TOXICITY.summaryScore.value > 0.5) {
+                removeClass();
+                document.querySelector('#bad_result_test').classList.add('show');
+            } else {
+                removeClass();
+                document.querySelector('#good_result_test').classList.add('show');
+            }
+        })
+        .catch(error => {
+            console.error("Erro ao analisar o texto:", error);
+        });
+});
+
+function removeClass() {
+    document.querySelector('#error_comment').classList.remove('show');
+    document.querySelector('#good_result_test').classList.remove('show');
+    document.querySelector('#bad_result_test').classList.remove('show');
+};
